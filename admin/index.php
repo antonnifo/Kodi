f5e8670313b59779fd12f1366dad9aff01d3bf1a
antonnifo <antonnifo@live.com>
2020-03-15 20:47:29 +0300

    return weather section

--- admin/index.php
+++ admin/index.php
@@ -42,7 +42,7 @@
                                     <li class="dropdown profile_details_drop">
                                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                                             <div class="profile_img">   
-                                                <span class="prfil-img"><img src="images/admin.jpg" alt=""> </span> 
+                                                <span class="prfil-img"><img src="../static/images/admin.jpg" alt=""> </span> 
                                             </div>  
                                         </a>
                                         <ul class="dropdown-menu drp-mnu">
@@ -190,49 +190,7 @@
                        </div>
                      <!-- //agile_top_w3_grids-->
                         <!-- /agile_top_w3_post_sections-->
-                            <div class="agile_top_w3_post_sections">
-                                   <div class="col-md-6 agile_top_w3_post agile_info_shadow">
-                                           <div class="img_wthee_post">
-                                                 <h3 class="w3_inner_tittle">Latest Report</h3>
-                                                <div class="stats-wrap">
-                                                    <div class="count_info"><h4 class="count">65,800,587 </h4><span class="year">Total Properties</span></div>
-                                                    <div class="year-info"><p class="text-no">20 </p><span class="year">This Year</span></div>
-                                                    <div class="clearfix"></div>
-                                                </div>
-                                                <div class="stats-wrap">
-                                                    <div class="count_info"><h4 class="count">2,690 </h4><span class="year">Total Properties</span></div>
-                                                    <div class="year-info"><p class="text-no">40 </p><span class="year">This Month</span></div>
-                                                    <div class="clearfix"></div>
-                                                </div>
-                                                <div class="stats-wrap">
-                                                    <div class="count_info"><h4 class="count">24,660 </h4><span class="year">Total Properties</span></div>
-                                                    <div class="year-info"><p class="text-no">30 </p><span class="year">This Week</span></div>
-                                                    <div class="clearfix"></div>
-                                                </div>
-                                                <div class="stats-wrap">
-                                                    <div class="count_info"><h4 class="count">1,204</h4><span class="year">Total Properties</span></div>
-                                                    <div class="year-info"><p class="text-no">10 </p><span class="year">This Day</span></div>
-                                                    <div class="clearfix"></div>
-                                                </div>
-                                              
-                                            </div>
-                                       </div>
-                                        <div class="col-md-6 agile_top_w3_post_info agile_info_shadow">
-                                            <div class="img_wthee_post1">
-                                            <h3 class="w3_inner_tittle"> Hours Working</h3>
-                                                <div class="main-example">
-                                                    <div class="clock"></div>
-                                                    <div class="message"></div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                       <div class="clearfix"></div>
-                                 </div>
-                                   
-
-                         <!--/prograc-blocks_agileits-->
-                            <div class="prograc-blocks_agileits">
+                       
                                 
                                 
                                  <div class="col-md-6 bars_agileits agile_info_shadow">
@@ -284,7 +242,75 @@
                                 </div>
                                      <div class="clearfix"></div>
                             </div>
-
+						<!-- /weather_w3_agile_info-->
+						<div class="weather_w3_agile_info agile_info_shadow">
+						  <div class="weather_w3_inner_info">
+						      
+							     <div class="over_lay_agile">
+								  <h3 class="w3_inner_tittle">Weather Report</h3>
+						       	  <ul>
+									<li>
+										<figure class="icons">
+											<canvas id="partly-cloudy-day" width="60" height="60"></canvas>
+										</figure>
+										<h3>25 °C</h3>
+										<div class="clearfix"></div>
+									</li>
+									<li>
+										<figure class="icons">
+											<canvas id="clear-day" width="60" height="60"></canvas>
+										</figure>
+										<div class="weather-text">
+											<h4>WED</h4>
+											<h5>27 °C</h5>
+										</div>
+										<div class="clearfix"></div>
+									</li>
+									<li>
+										<figure class="icons">
+											<canvas id="snow" width="60" height="60"></canvas>
+										</figure>
+										<div class="weather-text">
+											<h4>THU</h4>
+											<h5>13 °C</h5>
+										</div>
+										<div class="clearfix"></div>
+									</li>
+									<li>
+										<figure class="icons">
+											<canvas id="partly-cloudy-night" width="60" height="60"></canvas>
+										</figure>
+										<div class="weather-text">
+											<h4>FRI</h4>
+											<h5>18 °C</h5>
+										</div>
+										<div class="clearfix"></div>
+									</li>
+									<li>
+										<figure class="icons">
+											<canvas id="cloudy" width="60" height="60"></canvas>
+										</figure>
+										<div class="weather-text">
+											<h4>SAT</h4>
+											<h5>15 °C</h5>
+										</div>
+										<div class="clearfix"></div>
+									</li>
+									<li>
+										<figure class="icons">
+											<canvas id="fog" width="60" height="60"></canvas>
+										</figure>
+										<div class="weather-text">
+											<h4>SUN</h4>
+											<h5>11 °C</h5>
+										</div>
+										<div class="clearfix"></div>
+									</li>
+								</ul>
+								</div>
+							</div>	
+						</div>
+						<!-- //weather_w3_agile_info-->
 
 
                         
